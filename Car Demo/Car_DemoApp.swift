6380b0adead651447ba33b9c963f5b82de619240
Goyo <101522847+PeepoGeek@users.noreply.github.com>
2025-10-06 20:03:30 -0400

    Initial Commit

---
+++ Car Demo/Car_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Car_DemoApp.swift
+//  Car Demo
+//
+//  Created by Jose Gregorio Flores on 10/6/25.
+//
+
+import SwiftUI
+
+@main
+struct Car_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Car Demo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Car Demo
+//
+//  Created by Jose Gregorio Flores on 10/6/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
